386e46431b9d945dd80fecb0ad2f3bf5e5850ef2
Anzhelika Pikulina <angela.pikulina@gmail.com>
2020-06-09 16:06:22 +0200

    Initial Commit

---
+++ HW_2.7_TableView_Persons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_2.7_TableView_Persons
+//
+//  Created by 1 on 09.06.2020.
+//  Copyright © 2020 Anzhelika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
